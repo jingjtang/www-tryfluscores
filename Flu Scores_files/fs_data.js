697db5993af31dc03365e7c1397ce65ad18ae2d0
Jingjing Tang <31444565+jingjtang@users.noreply.github.com>
2019-09-06 13:20:40 -0400

    Add files via upload

--- Flu Scores_files/fs_data.js
+++ Flu Scores_files/fs_data.js
@@ -47,6 +47,15 @@
             }
             return results1;
           })();
+        } else if (season === 2018 || season === 20180) {
+          this.epiweeks = (function() {
+            var j, results1;
+            results1 = [];
+            for (i = j = 2; j <= 30; i = ++j) {
+              results1.push('' + (i <= 12 ? 201840 + i : 201900 + i - 12));
+            }
+            return results1;
+          })();
         } else {
           throw new Error('unsupported season: ' + season);
         }
@@ -166,6 +175,25 @@
             }
             return results1;
           }).call(this);
+        } else if (season === 20180) {
+          targets = (function() {
+            var j, len, results1;
+            results1 = [];
+            for (j = 0, len = targets.length; j < len; j++) {
+              t = targets[j];
+              results1.push(this.targets_2018[this.targets.indexOf(t)]);
+            }
+            return results1;
+          }).call(this);
+          regions = (function() {
+            var j, len, results1;
+            results1 = [];
+            for (j = 0, len = regions.length; j < len; j++) {
+              r = regions[j];
+              results1.push(this.regions_2018[this.regions.indexOf(r)]);
+            }
+            return results1;
+          }).call(this);
         }
         nr = regions.length;
         nt = targets.length;
@@ -238,16 +266,16 @@
         }
         for (j = 0, len = files.length; j < len; j++) {
           file = files[j];
-          if ((season === 2014 || season === 2015 || season === 2016 || season === 2017) && !file.name.endsWith('.zip')) {
+          if ((season === 2014 || season === 2015 || season === 2016 || season === 2017 || season === 2018) && !file.name.endsWith('.zip')) {
             return onFailure(`${file.name} is not a zip file`);
-          } else if ((season === 20150 || season === 20160 || season === 20170) && !file.name.endsWith('.csv')) {
+          } else if ((season === 20150 || season === 20160 || season === 20170 || season === 20180) && !file.name.endsWith('.csv')) {
             return onFailure(`${file.name} is not a csv file`);
           }
         }
         // load files one after another
         fileIndex = 0;
         data = {};
-        loadFunc = season === 20150 || season === 20160 || season === 20170 ? loadFull : loadSingle;
+        loadFunc = season === 20150 || season === 20160 || season === 20170 || season === 20180 ? loadFull : loadSingle;
         callback = function(name, fileData, error) {
           var t;
           if (error != null) {
@@ -308,6 +336,15 @@
       return results1;
     })();
 
+    FS_Data.regions_2018 = (function() {
+      var j, results1;
+      results1 = [];
+      for (i = j = 0; j <= 10; i = ++j) {
+        results1.push(i === 0 ? 'us' : `region${i}`);
+      }
+      return results1;
+    })();
+
     FS_Data.hhsRegions = (function() {
       var j, results1;
       results1 = [];
@@ -325,6 +362,8 @@
 
     FS_Data.targets_seasonal_2017 = ['onset', 'pkwk', 'pkper'];
 
+    FS_Data.targets_seasonal_2018 = ['onset', 'pkwk', 'pkper'];
+
     FS_Data.targets_local = ['1_week', '2_week', '3_week', '4_week'];
 
     FS_Data.targets_local_2015 = ['1wk', '2wk', '3wk', '4wk'];
@@ -333,6 +372,8 @@
 
     FS_Data.targets_local_2017 = ['1wk', '2wk', '3wk', '4wk'];
 
+    FS_Data.targets_local_2018 = ['1wk', '2wk', '3wk', '4wk'];
+
     FS_Data.targets = FS_Data.targets_seasonal.concat(FS_Data.targets_local);
 
     FS_Data.targets_2015 = FS_Data.targets_seasonal_2015.concat(FS_Data.targets_local_2015);
@@ -340,6 +381,8 @@
     FS_Data.targets_2016 = FS_Data.targets_seasonal_2016.concat(FS_Data.targets_local_2016);
 
     FS_Data.targets_2017 = FS_Data.targets_seasonal_2017.concat(FS_Data.targets_local_2017);
+
+    FS_Data.targets_2018 = FS_Data.targets_seasonal_2018.concat(FS_Data.targets_local_2018);
 
     FS_Data.errors = ['LS', 'AE'];
 
